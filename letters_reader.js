4e6463bbdbf838ef18f93dc95475f96c1c6241c6
SilverFoxxxy <ivan-belkov2@yandex.ru>
2021-08-02 16:57:44 +0300

    Update letters_reader.js

--- letters_reader.js
+++ letters_reader.js
@@ -20,6 +20,8 @@
 //     return null;
 // }
 
+const DEBUG = false;
+
 var urlParams = new URLSearchParams(window.location.search);
 // console.log(window.location.search);
 
@@ -31,9 +33,113 @@ var part_n = urlParams.get('part');
 // 	console.log("default - null");
 // }
 
+datastr='{"title": "Alice", "parts": [[{"text": "Завидев Алису, Кот только улыбнулся. Вид у него был добродушный, но когти длинные, а зубов так много, что Алиса сразу поняла, что с ним шутки плохи.", "from": 1}, {"text": "Котик! Чешик! ", "from": 2}, {"text": "робко начала Алиса. Она не знала, понравится ли ему это имя, но он только шире улыбнулся в ответ.", "from": 1}, {"text": "Ничего", "from": 2}, {"text": "подумала Алиса, – кажется, доволен. Вслух же она спросила:", "from": 1}, {"text": "Скажите, пожалуйста, куда мне отсюда идти?", "from": 2}, {"text": "А куда ты хочешь попасть?", "from": 3}, {"text": "ответил Кот", "from": 1}, {"text": "Мне все равно…", "from": 2}, {"text": "Тогда все равно, куда и идти", "from": 3}, {"text": "… только бы попасть куда-нибудь", "from": 2}, {"text": "Куда-нибудь ты обязательно попадешь, нужно только достаточно долго идти", "from": 3}, {"text": "С этим нельзя было не согласиться. Алиса решила переменить тему.", "from": 1}], [{"text": "А что здесь за люди живут?", "from": 2}, {"text": "Вон там живет Болванщик. А там Мартовский заяц. Все равно, к кому ты пойдешь. Оба не в своем уме", "from": 3}, {"text": "На что мне безумцы?", "from": 2}, {"text": "Ничего не поделаешь, все мы здесь не в своем уме – и ты, и я", "from": 3}]]}';
+
 var page_n = urlParams.get('page');
+var max_page = 1;
 
 var ispartloaded = -1;
+
+var data;
+
+var title = null;
+
+var textes;
+
+function parse_txt(txt) {
+	var from = txt["from"];
+	var pref = '';
+	var suf = '';
+	if (from == 1) {
+		pref = '<div class="center_block"><div class="center_msg">';
+		suf = '</div></div>';
+	}
+	if (from == 2) {
+		pref = '<div class="left_block"><div class="left_msg">';
+		suf = '</div></div>';
+	}
+	if (from == 3) {
+		pref = '<div class="right_block"><div class="right_msg">';
+		suf = '</div></div>';
+	}
+	console.log(txt);
+	return (pref + txt["text"] + suf);
+}
+
+function parse_page(partjs) {
+	console.log(partjs);
+	html = '<table>';
+	for (txt in partjs) {
+		console.log(txt);
+		nowtxt = parse_txt(partjs[txt])
+		html = html + '<tr><td>' + nowtxt + '</td></tr>';
+		html += '<tr><td><div class="left_block"><br></div></td></tr>';
+	}
+	html = html + '</table>';
+	return html;
+}
+
+async function reload_page() {
+	// document.getElementById("title").innerHTML = title;
+	if (ispartloaded == -1) {
+		if (DEBUG) {
+			data = await(JSON.parse(datastr));
+		} else {
+			name = "letters_example";
+			data = await fetch(name + ".json");
+		}
+	}
+	if (part_n == null) {
+		part_n = 0;
+	}
+	if (page_n == null || page_n < 0) {
+		page_n = 0;
+	}
+	if (page_n >= data["parts"].length) {
+		page_n = data["parts"].length - 1;
+	}
+	title = data["title"];
+	document.getElementById("title").innerHTML = title;
+	// TODO: название главы
+	// TODO: тома (несколько глав)
+	var nowpage = parse_page(data["parts"][page_n]);
+
+	// console.log(nowpage);
+
+	document.getElementById("messages_block").innerHTML = nowpage;
+	// html = json2page(data);
+	// title = json2title(data);
+}
+
+// TODO: fetch.await
+// data = $.getJSON(name + ".json", function(test) {
+// 	console.log( "success" );
+//  	data = test;
+//  	console.log(JSON.stringify(test));
+//  	$.each(data, function (key, val) {
+//  		if (key == 'title') {
+//  			title = val.data;
+//  		}
+//  		// items.push('<li id="' + key + '">' + val.data + ' ' + val.type + '</li>');
+//  	});
+//   	reload_page();
+// });
+
+reload_page();
+
+document.getElementById("prev_page_button").onclick = function()
+{
+  page_n--;
+  reload_page();
+}
+
+document.getElementById("next_page_button").onclick = function()
+{
+  page_n++;
+  reload_page();
+}
+
+// console.log(JSON.stringify(data));
 
 // const fs = require('fs');
 
@@ -44,15 +150,6 @@ var ispartloaded = -1;
 //   }
 //   console.log('File data:', jsonString)
 // })
-
-var data;
-data = $.getJSON("letters_example.json", function(test) {
- console.log( "success" );
- data = test;
- console.log(JSON.stringify(test));
-});
-
-
 
 // $.ajax({
 //   url: "http://cors.io/?https://github.com/SilverFoxxxy/SilverFoxxxy.github.io/letters_example.json",

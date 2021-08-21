323f63ca788799462f187908e9eaf8d7c9b54e2a
SilverFoxxxy <ivan-belkov2@yandex.ru>
2021-08-21 13:43:47 +0300

    json colorful

--- text_to_json.js
+++ text_to_json.js
@@ -18,6 +18,25 @@ function getCookie(name) {
     return null;
 }
 
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
 // filename = "skazhesh_mne"
 // f = open("textes/" + filename + ".txt", "r")
 var text = "EXAMPLE!";// f.read()
@@ -260,7 +279,7 @@ function parse(text) {
 
     let myString = JSON.stringify(letters, null, '\t');
 
-    document.getElementById("text1").innerHTML = myString;
+    document.getElementById("text1").innerHTML = syntaxHighlight(myString);
 
     console.log(letters);
 
